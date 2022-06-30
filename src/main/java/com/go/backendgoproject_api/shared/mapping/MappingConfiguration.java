9fecab2735aa499aa8cfaa169b28514e1b72a8e1
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-29 23:02:09 -0500

    Update MappingConfiguration.java

--- src/main/java/com/go/backendgoproject_api/shared/mapping/MappingConfiguration.java
+++ src/main/java/com/go/backendgoproject_api/shared/mapping/MappingConfiguration.java
@@ -1,4 +1,5 @@
 package com.go.backendgoproject_api.shared.mapping;
+import com.go.backendgoproject_api.block.mapping.BlockMapper;
 import com.go.backendgoproject_api.transaction.mapping.TransactionMapper;
 import com.go.backendgoproject_api.user.mapping.UserMapper;
 import org.springframework.context.annotation.Bean;
@@ -21,4 +22,8 @@ public class MappingConfiguration {
         return new TransactionMapper();
     }
 
+    @Bean
+    public BlockMapper blockMapper() {
+        return new BlockMapper();
+    }
 }
